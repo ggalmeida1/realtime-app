4a5257feaed50c771e16f7eb7e8198f28d5670a0
Gabriel Almeida <biel88sud@gmail.com>
2023-08-10 16:23:26 -0300

    Check error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { Redis } from '@upstash/redis'
 
 export const db = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
+  url: process.env.UPSTASH_REDIS_REST_URL, //https://bobbyhadz.com/blog/typescript-no-overload-matches-this-call
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
