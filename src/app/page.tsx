3a0b1b8a86eb4ba6c1aa95cafc2d0cbaa6eaf180
Gabriel Almeida <biel88sud@gmail.com>
2023-08-10 17:27:11 -0300

    Fixing breaking changes on the icon lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { db } from '@/lib/db';
+import Button from './components/ui/Button';
 
 export default async function Home() {
   await db.set("hello", "hello");
-  return <div className="text-red-500">Hello world</div>;
+  return <Button>hell0</Button>
 }
